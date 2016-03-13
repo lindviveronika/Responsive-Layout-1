09450222338d6801e9719e88117e92a32d71b63c
lindviveronika <lindviveronika@gmail.com>
2016-03-13 13:52:49 +0100

    Remove automatic sliding in the gallery on mobile

--- js/main.js
+++ js/main.js
@@ -92,9 +92,11 @@ $(document).ready(function(){
 	}
 
 	function startSlider(){
-		interval = setInterval(function(){
-			slideRight();
-		}, pause);
+		if(!isMobile.any()){
+			interval = setInterval(function(){
+				slideRight();
+			}, pause);
+		}
 	};
 
 	function stopSlider(){
@@ -142,3 +144,27 @@ jQuery.extend( jQuery.easing,
 		return -c/2 * ((--t)*(t-2) - 1) + b;
 	},
 });
+
+/*  IS MOBILE ============================================================================= */
+
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+
